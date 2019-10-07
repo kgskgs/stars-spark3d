dafecb1c1e905a0006adf5050edd1fe55c8e18da
kgskgs <kgs.void@gmail.com>
2019-10-07 12:03:12 +0300

    update .gitignore

--- src/gravity_test.cmd
+++
@@ -1,5 +0,0 @@
-del /Q D:\tmp\spark-events\*
-
-call start spark-submit --name "7 - gravity_udf.py" src/gravity_udf.py --limit 7
-call start spark-submit --name "7 - gravity_rdds.py" src/gravity_rdds.py --limit 7
-call start spark-submit --name "7 - gravity_accumulator.py" src/gravity_accumulator.py --limit 7
